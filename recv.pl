56e6b5e1cf3f703fe2fe3d78c998f12949fd0816
Mariusz Gronczewski <xani666@gmail.com>
2013-07-06 22:10:38 +0200

    moar parameters

--- recv.pl
+++ recv.pl
@@ -4,22 +4,32 @@ use EV;
 use POE qw( Loop::AnyEvent );
 use POE::Component::Client::AMQP;
 use Data::Dumper;
-my $queue_name = 'test_queue2';
+my $queue_name = 'test_queue4';
 
 
-Net::AMQP::Protocol->load_xml_spec('amqp0-8.xml');
+Net::AMQP::Protocol->load_xml_spec('amqp0-9-1.xml');
 
 my $end = AnyEvent->condvar;
 my $amq = POE::Component::Client::AMQP->create(
     RemoteAddress => 'd01.home.zxz.li',
 );
 
-
-$amq->channel(1)->queue($queue_name, { auto_delete => 0,  exclusive => 0})->subscribe(
+my $i;
+$amq->channel(1)->queue($queue_name,
+                        {
+                            auto_delete => 0,
+                            exclusive => 0,
+                            durable => 0,
+                            arguments => {
+                                'x-message-ttl' => 20 * 1000,
+                                'x-expires' => 60 * 1000,
+                                'x-ha-policy' => 'all',
+                            },
+                        })->subscribe(
     sub {
         my ($payload, $meta) = @_;
         my $reply_to = $meta->{header_frame}->reply_to;
-        print "Msg received: $payload\n";
+#        print ++$i .  " Msg received: $payload\n";
 #        print Dumper $payload;
 #        print Dumper $meta;
 #        $amq->channel(1)->queue($reply_to)->publish("Message received");
--- test.pl
+++ test.pl
@@ -4,10 +4,10 @@ use EV;
 use POE qw( Loop::AnyEvent );
 use POE::Component::Client::AMQP;
 use Data::Dumper;
-my $queue_name = 'test_queue2';
+my $queue_name = 'test_queue4';
 
 
-Net::AMQP::Protocol->load_xml_spec('amqp0-8.xml');
+Net::AMQP::Protocol->load_xml_spec('amqp0-9-1.xml');
 
 my $end = AnyEvent->condvar;
 my $amq = POE::Component::Client::AMQP->create(
@@ -22,6 +22,12 @@ my $queue = $channel->queue(
     {
         auto_delete => 0, # will remain after all consumers part
         exclusive => 0, # not limited to just this connection
+        durable => 0,
+        arguments => {
+            'x-message-ttl' => 20 * 1000,
+            'x-expires' => 60 * 1000,
+            'x-ha-policy' => 'all',
+        },
     },
 );
 
@@ -30,7 +36,9 @@ my $w = AnyEvent->timer (
     interval => 1,
     cb       => sub {
         print "Sending msg\n";
-        $queue->publish('test');
+        foreach(my $a =0 ; $a<1000 ; ++$a) {
+            $queue->publish('test');
+        }
     },
 );
 
